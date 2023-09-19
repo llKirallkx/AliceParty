5b1981b6eddf410abad1c52eeaafc0e634f38903
Tiago Assis <96582871+llKirallkx@users.noreply.github.com>
2023-09-19 11:30:21 -0300

    add banner

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+// function getRandomColor() {
+//     const letters = '0123456789ABCDEF';
+//     let color = '#';
+//     for (let i = 0; i < 6; i++) {
+//         color += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return color;
+// }
 
-function changeBackgroundColor() {
-    document.body.style.backgroundColor = getRandomColor();
-}
+// function changeBackgroundColor() {
+//     document.body.style.backgroundColor = getRandomColor();
+// }
 
-setInterval(changeBackgroundColor, 1000);
+// setInterval(changeBackgroundColor, 1000);
